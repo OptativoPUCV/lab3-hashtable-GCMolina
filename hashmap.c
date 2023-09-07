a64d944312136f0e4f4a0133f47fbdbfbb34c11d
Replit user <>
2023-09-07 23:07:53 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -78,7 +78,23 @@ HashMap * createMap(long capacity) {
 }
 
 void eraseMap(HashMap * map,  char * key) {
-
+  if(map==NULL || key == NULL){
+    return;
+  }
+  long posicion = hash(key , map->capacity);
+  while(map->buckets[posicion]!= NULL){
+    Pair* aux = map->buckets[posicion];
+    if(is_equal(aux->key,key)){
+      aux->key=NULL;
+      map->size--;
+      return;
+    }
+    posicionn=(poscinion+1)%map->capacity;
+    if(posicion==map->current){
+      break;
+    }
+  }
+  
 }
 
 Pair * searchMap(HashMap * map,  char * key) {   
