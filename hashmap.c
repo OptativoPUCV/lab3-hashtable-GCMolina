c46ba5796703c8f8080eb7d14c0e7c76310ec27f
Replit user <>
2023-09-08 23:45:37 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -120,7 +120,12 @@ Pair * searchMap(HashMap * map,  char * key) {
 }
 
 Pair * firstMap(HashMap * map) {
-
+  for(long i=0;i<map->capacity;i++){
+    if(map->buckets!=NULL && map->buckets->key){
+      map
+    }
+    
+  }
     return NULL;
 }
 
